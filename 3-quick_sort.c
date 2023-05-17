de6e075b024dd5c94ef60213a72c5569deb3c056
BryanField15 <6367@holbertonstudents.com>
2023-05-17 04:06:58 -0700

    start of final task

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	
+}
