8d6b3273dfc240dfbe88af00d4f0fecd6fc8bf11
BryanField15 <6367@holbertonstudents.com>
2023-05-17 03:06:40 -0700

    refactored

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,60 @@
 #include "sort.h"
+/**
+ * list_length - determines the length of a list
+ * @list: list to be evaluated
+ * Return: length
+ */
+
+size_t list_length(listint_t **list)
+{
+	size_t length = 0;
+	listint_t *temp;
+
+	if (list == NULL || (*list) == NULL)
+	{
+		return (0);
+	}
+	temp = *list;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		length = length + 1;
+	}
+	return (length);
+}
+
+/**
+ * swap_node - swaps any two nodes
+ * @list: list containing nodes to be swapped
+ * @temp: one node to be swapped
+ * @sectemp: other node to be swapped
+ * Return: void
+ */
+
+void swap_node(listint_t **list, listint_t *temp, listint_t *sectemp)
+{
+	if (temp->prev == NULL)
+	{
+		*list = sectemp;
+		sectemp->prev = NULL;
+	}
+	else
+	{
+		temp->prev->next = sectemp;
+		sectemp->prev = temp->prev;
+	}
+	if (sectemp->next == NULL)
+	{
+		temp->next = NULL;
+	}
+	else
+	{
+		sectemp->next->prev = temp;
+		temp->next = sectemp->next;
+	}
+	temp->prev = sectemp;
+	sectemp->next = temp;
+}
 
 /**
  * insertion_sort_list - sort linked list using insertion method
@@ -10,72 +66,25 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp;
 	listint_t *sectemp;
-	int k;
 
-	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
+	if (list_length(list) < 2)
 	{
 		return;
 	}
 
 	temp = *list;
-	sectemp = temp->next;
-	if (temp->prev == NULL && sectemp->next == NULL)
+	while (temp->next != NULL)
 	{
-		if (temp->n >= sectemp->n)
-		{
-			*list = sectemp;
-			sectemp->prev = NULL;
-			temp->prev = sectemp;
-			sectemp->next = temp;
-			temp->next = NULL;
-			print_list(*list);
-
-		}
-		return;
-	}
-	k = 0;
-	while (sectemp->next != NULL)
-	{
-		while (k == 0)
-		{
-			if (temp->n > sectemp->n)
-			{
-				if (temp->prev == NULL)
-				{
-					*list = sectemp;
-					sectemp->prev = NULL;
-				}
-				else
-				{
-					temp->prev->next = sectemp;
-					sectemp->prev = temp->prev;
-				}
-				if (sectemp->next == NULL)
-				{
-					temp->next = NULL;
-				}
-				else
-				{
-					sectemp->next->prev = temp;
-					temp->next = sectemp->next;
-				}
-				temp->prev = sectemp;
-				sectemp->next = temp;
-				print_list(*list);
-				k = 1;
-			}
-			else
-			{
-				temp = temp->next;
-				sectemp = sectemp->next;
-				if (temp->next == NULL)
-				{
-					return;
-				}
-			}
-		}
-		temp = *list;
 		sectemp = temp->next;
-		k = 0;
+		if (temp->n > sectemp->n)
+		{
+			swap_node(list, temp, sectemp);
+			print_list(*list);
+			temp = *list;
 		}
+		else
+		{
+			temp = temp->next;
+		}
+	}
 }
