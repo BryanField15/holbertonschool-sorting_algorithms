84b4590cf6e510da54f39f37cc901145e3b9a2cb
angellalao <6369@holbertonstudents.com>
2023-05-18 03:27:32 -0700

    test

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,5 +9,24 @@
 
 void quick_sort(int *array, size_t size)
 {
+	size_t pivot;
+	size_t j;
+	size_t z;
+	size_t temp;
 	
+	j = 0;
+	z = size - 1;
+	pivot = size/2;
+	while (j < z)
+	{
+		if (array[z] < array[pivot])
+		{
+			temp = array[z];
+			array[z] = array[j];
+			array[j] = temp;
+			j = j + 1;
+			print_array(array, size);
+		}
+		z = z - 1;
+	}
 }
